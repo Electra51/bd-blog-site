84e8d08963f66a8d6f45b662d3f541e7050144c9
Electra51 <safayetnurelectra@gmail.com>
2022-09-04 12:13:44 +0600

    blogs udated

--- blogs.js
+++ blogs.js
@@ -4,26 +4,170 @@ const loadCatagories = async() => {
     displayCatagories(data.data.news_category);
 }
 const displayCatagories = category => {
-  console.log(category[0].category_name)
+  // console.log(category[0].category_name)
   const catagoryContainer = document.getElementById('catagory');
 
   catagoryContainer.innerHTML = `
     <div>
-    <button class="btn btn-outline-primary ms-5">${category[0].category_name}</button>
-    <button class="btn btn-outline-primary ms-3">${category[1].category_name}</button>
-    <button class="btn btn-outline-primary ms-3">${category[2].category_name}</button>
-    <button class="btn btn-outline-primary ms-3">${category[3].category_name}</button>
-    <button class="btn btn-outline-primary ms-3">${category[4].category_name}</button>
-    <button class="btn btn-outline-primary ms-3">${category[5].category_name}</button>
-    <button class="btn btn-outline-primary ms-3">${category[6].category_name}</button>
-    <button class="btn btn-outline-primary ms-3">${category[7].category_name}</button>
+    <button onclick="loadCategory1()" class="btn btn-outline-primary ms-5 my-2">${category[0].category_name}</button>
+    <button onclick="loadCategory2()" class="btn btn-outline-primary ms-3 my-2">${category[1].category_name}</button>
+    <button onclick="loadCategory3()" class="btn btn-outline-primary ms-3 my-2">${category[2].category_name}</button>
+    <button class="btn btn-outline-primary ms-3 my-2">${category[3].category_name}</button>
+    <button class="btn btn-outline-primary ms-3 my-2">${category[4].category_name}</button>
+    <button class="btn btn-outline-primary ms-3 my-2">${category[5].category_name}</button>
+    <button class="btn btn-outline-primary ms-3 my-2">${category[6].category_name}</button>
+    <button class="btn btn-outline-primary ms-3 my-2">${category[7].category_name}</button>
     </div>
   
   `;
 
 }
 
-
-
   loadCatagories();
 
+  const loadCategory1 = async() => {
+    const response = await fetch("https://openapi.programming-hero.com/api/news/category/01");
+    const data = await response.json();
+    displayCategory1 (data.data);
+}
+const displayCategory1 = datas => {
+  
+    const divContainer = document.getElementById('news-card');
+    for(const _id of datas){
+        console.log(_id);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `<div class="card">
+        <img src="${_id.thumbnail_url}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${_id.title}</h5>
+          <p class="card-text">${_id.details}</p>
+          
+          <div class="d-flex">
+          <img src="${_id.author.img}"alt="" width="30" height="29" class="d-inline-block align-text-top">
+          <p class="ps-2">${_id.author.name}</p>
+          <i class="fa fa-solid fa-eye mt-1 ps-5">${_id.total_view}</i>
+          <button class="btn btn-primary ms-5" data-bs-toggle="modal" data-bs-target="#newsdetailmodal">See more</button>
+          <div class="modal fade" id="newsdetailmodal" tabindex="-1" aria-labelledby="newsdetailmodalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="newsdetailmodalLabel">${_id.title}</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <img src="${_id.thumbnail_url}" class="card-img-top" alt="...">
+      <p class="card-text">${_id.details}</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        
+      </div>
+    </div>
+  </div>
+</div>
+  
+          </div>
+        </div>
+      </div>`;
+        divContainer.appendChild(div);
+    }
+}
+ 
+const loadCategory2 = async() => {
+  const response = await fetch("https://openapi.programming-hero.com/api/news/category/02");
+  const data = await response.json();
+  displayCategory2 (data.data);
+}
+const displayCategory2 = datas => {
+
+  const divContainer = document.getElementById('news-card');
+  for(const _id of datas){
+      console.log(_id);
+      const div = document.createElement('div');
+      div.classList.add('col');
+      div.innerHTML = `<div class="card">
+      <img src="${_id.thumbnail_url}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${_id.title}</h5>
+        <p class="card-text">${_id.details}</p>
+        
+        <div class="d-flex">
+        <img src="${_id.author.img}"alt="" width="30" height="29" class="d-inline-block align-text-top">
+        <p class="ps-2">${_id.author.name}</p>
+        <i class="fa fa-solid fa-eye mt-1 ps-5">${_id.total_view}</i>
+        <button class="btn btn-primary ms-5" data-bs-toggle="modal" data-bs-target="#newsdetailmodal">See more</button>
+        <div class="modal fade" id="newsdetailmodal" tabindex="-1" aria-labelledby="newsdetailmodalLabel" aria-hidden="true">
+<div class="modal-dialog">
+  <div class="modal-content">
+    <div class="modal-header">
+      <h5 class="modal-title" id="newsdetailmodalLabel">${_id.title}</h5>
+      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    </div>
+    <div class="modal-body">
+    <img src="${_id.thumbnail_url}" class="card-img-top" alt="...">
+    <p class="card-text">${_id.details}</p>
+    </div>
+    <div class="modal-footer">
+      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+      
+    </div>
+  </div>
+</div>
+</div>
+
+        </div>
+      </div>
+    </div>`;
+      divContainer.appendChild(div);
+  }
+}
+
+const loadCategory3 = async() => {
+  const response = await fetch("https://openapi.programming-hero.com/api/news/category/03");
+  const data = await response.json();
+  displayCategory3 (data.data);
+}
+const displayCategory3 = datas => {
+
+  const divContainer = document.getElementById('news-card');
+  for(const _id of datas){
+      console.log(_id);
+      const div = document.createElement('div');
+      div.classList.add('col');
+      div.innerHTML = `<div class="card">
+      <img src="${_id.thumbnail_url}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${_id.title}</h5>
+        <p class="card-text">${_id.details}</p>
+        
+        <div class="d-flex">
+        <img src="${_id.author.img}"alt="" width="30" height="29" class="d-inline-block align-text-top">
+        <p class="ps-2">${_id.author.name}</p>
+        <i class="fa fa-solid fa-eye mt-1 ps-5">${_id.total_view}</i>
+        <button class="btn btn-primary ms-5" data-bs-toggle="modal" data-bs-target="#newsdetailmodal">See more</button>
+        <div class="modal fade" id="newsdetailmodal" tabindex="-1" aria-labelledby="newsdetailmodalLabel" aria-hidden="true">
+<div class="modal-dialog">
+  <div class="modal-content">
+    <div class="modal-header">
+      <h5 class="modal-title" id="newsdetailmodalLabel">${_id.title}</h5>
+      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    </div>
+    <div class="modal-body">
+    <img src="${_id.thumbnail_url}" class="card-img-top" alt="...">
+    <p class="card-text">${_id.details}</p>
+    </div>
+    <div class="modal-footer">
+      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+      
+    </div>
+  </div>
+</div>
+</div>
+
+        </div>
+      </div>
+    </div>`;
+      divContainer.appendChild(div);
+  }
+}
--- news.js
+++ news.js
@@ -1,29 +1,24 @@
 const loadNews = async() => {
-    const response = await fetch("https://openapi.programming-hero.com/api/news/category/01");
+    const response = await fetch("https://openapi.programming-hero.com/api/news/0282e0e58a5c404fbd15261f11c2ab6a");
   const data = await response.json();
-  console.log(data.data);
+  displayNews (data.data);
     // displayCatagories(data.data.news_category);
 }
 const displayNews = newses => {
- console.log(newses)
-  const newsContainer = document.getElementById('news-container');
+ console.log(newses);
+  const newsContainer = document.getElementById('news_container');
     newses.forEach(news => {
         const newsDiv = document.createElement('div');
         newsDiv.classList.add('col');
-        newsDiv.innerHTML = `<div class="col-lg-6 col-md-12">
-    <div class=" border rounded bg-light d-flex flex-md-row flex-sm-column ">
-      <img src="${image_url}" alt="">
-      <div class="ms-3 p-3">
-          
-           <h5 class="fw-bold">Digital marketing</h5>
-      <p class="text-black-50 lh-sm">Join millions of learners from around the world already learning</p>
-          <h6 class="text-primary">Price : 40$</h6>
+        newsDiv.innerHTML = `<div class="card">
+          <img src="${image_url}" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
           </div>
-      </div>
-    
-  </div>`;
+        </div>`;
         newsContainer.appendChild(newsDiv);
-        //     console.log(catagories.category_name);
+        // console.log(catagories.category_name);
     });
   
 }
