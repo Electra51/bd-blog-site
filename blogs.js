276299e3119d0e0eff75b3133ff47c385d765ab1
Electra51 <safayetnurelectra@gmail.com>
2022-09-04 15:51:29 +0600

    sppinner added

--- blogs.js
+++ blogs.js
@@ -25,21 +25,28 @@ const displayCatagories = category => {
 
   loadCatagories();
 
-  const loadCategory1 = async(catagory) => {
+const loadCategory1 = async (catagory) => {
+  toggleSpinner(true);
     const response = await fetch(`https://openapi.programming-hero.com/api/news/category/0${catagory}`);
     const data = await response.json();
     displayCategory1 (data.data);
 }
 const displayCategory1 = datas => {
+
+  const display = document.getElementById('search')
+display.innerHTML = `
+
+  <input type="text" class="form-control mt-1" placeholder="${datas.length} information show" aria-label="Username" aria-describedby="basic-addon1">
+`
   
   const divContainer = document.getElementById('news-card');
   divContainer.innerHTML = ``;
 
-    for(const _id of datas){
+  for (const _id of datas) {
         console.log(_id);
         const div = document.createElement('div');
         div.classList.add('col');
-        div.innerHTML = `<div class="card">
+        div.innerHTML = `<div class="card ">
         <img src="${_id.thumbnail_url}" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">${_id.title}</h5>
@@ -50,13 +57,42 @@ const displayCategory1 = datas => {
           <p class="ps-2">${_id.author.name}</p>
           <i class="fa fa-solid fa-eye mt-1 ps-5">${_id.total_view}</i>
           <button class="btn btn-primary ms-5" data-bs-toggle="modal" data-bs-target="#newsdetailmodal">See more</button>
-          </div>
-          </div>
+          
 </div>
+<div class="modal fade" id="newsdetailmodal" tabindex="-1" aria-labelledby="newsdetailmodalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="newsdetailmodalLabel">${_id.title}</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <img src="${_id.thumbnail_url}" class="card-img-top" alt="...">
+      <p class="card-text">${_id.details}</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        
+      </div>
+    </div>
+  </div>
+</div>
+
     </div>
         </div>
       </div>`;
         divContainer.appendChild(div);
-    }
+  }
+  toggleSpinner(false);
 }
  
+
+const toggleSpinner = isLoading => {
+  const loaderSection = document.getElementById('loader');
+  if(isLoading){
+      loaderSection.classList.remove('d-none')
+  }
+  else{
+      loaderSection.classList.add('d-none');
+  }
+}
